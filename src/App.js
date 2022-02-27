d2501998b4a47779cd407244524ef036658da0ee
Amal Jaison <amaljaison71@gmail.com>
2022-02-27 12:33:52 +0530

    [FIX] authentication state reference in app module

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Auth from "./components/Auth";
 import UserProfile from "./components/UserProfile";
 
 function App() {
-  const isAuth = useSelector((state) => state.auth.isAuhtenticated);
+  const isAuth = useSelector((state) => state.auth.isAuthenticated);
   return (
     <React.Fragment>
       <Header />
